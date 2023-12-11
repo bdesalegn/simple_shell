472850b191fc5bd273b0289557df37e581d135f6
Biset Desalegn <bisetsamara@gmail.com>
2023-12-10 22:12:28 -0800

    update varse.c

--- varse.c
+++ varse.c
@@ -16,18 +16,18 @@ int is_chain_delimiter(info_t *info, char *buffer, size_t *position)
 	{
 		buffer[j] = 0;
 		j++;
-		info->commandBufferType = CMD_OR;
+		info->cmd_buf_type = CMD_OR;
 	}
 	else if (buffer[j] == '&' && buffer[j + 1] == '&')
 	{
 		buffer[j] = 0;
 		j++;
-		info->commandBufferType = CMD_AND;
+		info->cmd_buf_type = CMD_AND;
 	}
 	else if (buffer[j] == ';') /* found end of this command */
 	{
 		buffer[j] = 0; /* replace semicolon with null */
-		info->commandBufferType = CMD_CHAIN;
+		info->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
@@ -50,7 +50,7 @@ void checkDelimiter(info_t *info, char *buffer, size_t *position,
 {
 	size_t j = *position;
 
-	if (info->commandBufferType == CMD_AND)
+	if (info->cmd_buf_type == CMD_AND)
 	{
 		if (info->status)
 		{
@@ -58,7 +58,7 @@ void checkDelimiter(info_t *info, char *buffer, size_t *position,
 			j = length;
 		}
 	}
-	if (info->commandBufferType == CMD_OR)
+	if (info->cmd_buf_type == CMD_OR)
 	{
 		if (!info->status)
 		{
@@ -84,17 +84,17 @@ int replaceAlias(info_t *info)
 
 	for (i = 0; i < 10; i++)
 	{
-		node = cust_node_starts_with(info->alias, info->arguments[0], '=');
+		node = cust_node_starts_with(info->alias, info->argv[0], '=');
 		if (!node)
 			return (0);
-		free(info->arguments[0]);
+		free(info->argv[0]);
 		ptr = _findCharacter(node->str, '=');
 		if (!ptr)
 			return (0);
 		ptr = duplicateString(ptr + 1);
 		if (!ptr)
 			return (0);
-		info->arguments[0] = ptr;
+		info->argv[0] = ptr;
 	}
 	return (1);
 }
@@ -110,31 +110,31 @@ int replaceVariables(info_t *info)
 	int i = 0;
 	list_t *node;
 
-	for (i = 0; info->arguments[i]; i++)
+	for (i = 0; info->argv[i]; i++)
 	{
-		if (info->arguments[i][0] != '$' || !info->arguments[i][1])
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
 			continue;
 
-		if (!cust_strcmp(info->arguments[i], "$?"))
+		if (!cust_strcmp(info->argv[i], "$?"))
 		{
-			replaceString(&(info->arguments[i]),
+			replaceString(&(info->argv[i]),
 							duplicateString(convert_to_string(info->status, 10, 0)));
 			continue;
 		}
-		if (!cust_strcmp(info->arguments[i], "$$"))
+		if (!cust_strcmp(info->argv[i], "$$"))
 		{
-			replaceString(&(info->arguments[i]),
+			replaceString(&(info->argv[i]),
 						duplicateString(convert_to_string(getpid(), 10, 0)));
 			continue;
 		}
-		node = cust_node_starts_with(info->environment, &info->arguments[i][1], '=');
+		node = cust_node_starts_with(info->environ, &info->argv[i][1], '=');
 		if (node)
 		{
-			replaceString(&(info->arguments[i]),
+			replaceString(&(info->argv[i]),
 						duplicateString(_findCharacter(node->str, '=') + 1));
 			continue;
 		}
-		replaceString(&info->arguments[i], duplicateString(""));
+		replaceString(&info->argv[i], duplicateString(""));
 	}
 	return (0);
 }
